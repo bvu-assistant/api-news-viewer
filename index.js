1a3e9ea78e8ce57f92885c1ce7eb968f7d9c99f0
iceStorm <anhtuan3675@outlook.com>
2020-10-16 20:43:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const app = require('express')();
+const request = require('request');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.set('view engine', 'ejs');
+
+
+const PORT = 5500;
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
+    console.log('Open browser on: http://localhost:' + PORT);
+});
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Ok');
+});
+
+
+
+
+app.get('/:newsId', (req, res) => {
+    const newsId = req.params.newsId;
+
+
+    getNewsDetails(newsId)
+        .then(json => {
+
+            let actualLinks = [];
+            let actualMessage = json.fullMessage;
+
+
+            for (var link in json.links) {
+                actualLinks.push({
+                    title: link,
+                    url: 'https://sinhvien.bvu.edu.vn/' + json.links[link]
+                });
+
+                if (actualMessage.includes(link))
+                    actualMessage = actualMessage.split(link).join('\n');
+            }
+
+            res.render('index', {
+                message: actualMessage,
+                links: actualLinks
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.send('');
+        })
+});
+
+
+function getNewsDetails(id) {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'GET',
+            url: `http://bvu-news-getter.herokuapp.com/details/${id}`
+        },
+        (err, response, body) => {
+            if (err || response.statusCode !== 200) {
+                return resolve({});
+            }
+
+            try {
+                body = JSON.parse(body);
+                return resolve(body);
+            }
+            catch (err) {   //  không tìm thấy thông tin
+                return resolve({});
+            }
+        });
+    });
+}
